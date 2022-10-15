51fbad8f95053fe9ccd2000777ae0d0316671798
ANJALY22 <anjalyjayaraj16@gmail.com>
2022-10-15 13:15:37 +0530

    My first SPA project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ReactiveFormsModule,FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+const appRoute: Routes=[
+  {
+    path:'',component:SigninComponent 
+  },
+  {
+    path:'REGISTER',component:RegisterComponent
+  },
+  {
+    path:'SIGNUP',component:SigninComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
